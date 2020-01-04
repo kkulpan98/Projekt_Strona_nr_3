a5c21d69341de1a8fce3ec59784e93453df136c8
kamkul <kkulpan98@gmail.com>
2020-01-04 12:20:45 +0100

    Tera juz z gorki bedzie

--- jq/jquery.js
+++ jq/jquery.js
@@ -1,7 +1,12 @@
 $(document).ready(function () {
     initTooltips();
     prepareImageLoader();
+    applyFilter();
 });
+
+var image = new Image();
+var ctx;
+var canvas;
 
 function initTooltips() {
     $(document).ready(function () {
@@ -9,11 +14,24 @@ function initTooltips() {
     });
 }
 
+function applyFilter() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(image, 0, 0);
+
+    for (var i = 0; i < 1 && i * 300 < image.height - 11; i += 0.04) {
+        ctx.globalAlpha = i;
+        ctx.fillRect(0, i * 300, i * 300, 10);
+        // ctx.fillRect(0, image.width - i * 300,  image.height - i * 300, 10);
+    }
+
+}
+
 function prepareImageLoader() {
     var uploadImg = document.getElementById('uploadImg');
-    var outputImg = document.getElementById('outputImg');
+    canvas = document.getElementById("canvas");
+    ctx = canvas.getContext("2d");
 
-    uploadImg.addEventListener('change', function() {
+    uploadImg.addEventListener('change', function () {
         var file = this.files[0];
         if (file.type.indexOf('image') < 0) {
             return;
@@ -21,26 +39,47 @@ function prepareImageLoader() {
 
         var fReader = new FileReader();
 
-        fReader.onload = function() {
-            outputImg.src = fReader.result;
+        fReader.onload = function () {
 
-            localStorage.setItem("imgData", getBase64Image(outputImg));
+            // canvas.width = img.width;
+            // canvas.height = img.height;
+
+            image.src = fReader.result;
+
+            image.onload = function () {
+                // var sourceWidth = this.width;
+                // var sourceHeight = this.height;
+                // var destX = canvas.width / 2 - this.width / 2;
+                // var destY = canvas.height / 2 - this.height / 2;
+                ctx.drawImage(this, 0, 0);
+
+                // for (var i = 0; i < 1 && i * 300 < image.height - 11; i += 0.04) {
+                //             ctx.globalAlpha = i;
+                //             ctx.fillRect(0, i * 300, i * 300, 10);
+                //             // ctx.fillRect(0, image.width - i * 300,  image.height - i * 300, 10);
+                //         }
+
+                var dataURL = canvas.toDataURL("image/png");
+
+
+                localStorage.setItem("imgData", dataURL.replace(/^data:image\/(png|jpg);base64,/, ""));
+            };
         };
 
         fReader.readAsDataURL(file);
     });
 }
 
-function getBase64Image(img) {
-    var canvas = document.createElement("canvas");
-    canvas.width = img.width;
-    canvas.height = img.height;
-
-    var ctx = canvas.getContext("2d");
-    ctx.drawImage(img, 0, 0);
-
-    var dataURL = canvas.toDataURL("image/png");
-
-    return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
-}
+// function getBase64Image(img) {
+//     var canvas = document.createElement("canvas");
+//     canvas.width = img.width;
+//     canvas.height = img.height;
+//
+//     var ctx = canvas.getContext("2d");
+//     ctx.drawImage(img, 0, 0);
+//
+//     var dataURL = canvas.toDataURL("image/png");
+//
+//     return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+// }
 
